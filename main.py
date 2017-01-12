e4a09eddf2e31d1c9c052e4b0d7d5021eb5c3c11
Daniel Kaczmarczyk <danielkaczmarczyk2@gmail.com>
2017-01-12 18:59:08 +0000

    class definitions, renaming

--- main.py
+++ main.py
@@ -1,14 +1,9 @@
 # WELCOME CONTRIBUTORS, THIS IS A VERY EARLY STAGE, PLEASE EXCUSE THE INITIAL MESS ;)
 
-# some notes on how to store data
+import exercise, generator, scales
 
+def routine_printer(exercise, generator, scale):
+	print('Practice routine')
+	print(exercise.text)
+	print(exercise.time)
 
-#scales
-chromatic = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11]
-major = [0, 2, 4, 5, 7, 9, 11]
-minor = [0, 2, 3, 5, 7, 8, 10]
-blues = [0, 3, 5, 6, 7, 10]
-# every integer represents the distance from the root note
-# 0 is the root note, 7 is a perfect 5th, 3 is a minor 3rd etc
-# the possible intervals are from the root up until major 7th
-# using lists for scales and arpeggios
