cb7d9553a6b3a7164d211e163c79e760685ad713
Daniel Kaczmarczyk <danielkaczmarczyk2@gmail.com>
2017-01-14 21:45:18 +0000

    created the scale_to_fretboard method, works fine

--- main.py
+++ main.py
@@ -1,10 +1,30 @@
 # WELCOME CONTRIBUTORS, THIS IS A VERY EARLY STAGE, PLEASE EXCUSE THE INITIAL MESS ;)
 
-import exercise, generator, scales
+import arpeggio, exercise, generator, scales
 
-def routine_printer(exercise, generator, scale):
+arpeggio = arpeggio.Arpeggio('major', [0, 4, 7])
+
+def routine_printer(arpeggio, scale_gen):
 	'''test'''
 	print('Practice routine')
-	print(exercise.text)
-	print(exercise.time)
+	print('Arpeggio:', arpeggio.name, arpeggio.recipe)
+	print('Random scale:', scale_gen)
+	print('Alternate picking pattern:', generator.permutations_generator())
 
+
+#mockup of the output
+print('scale to practice: e minor')
+f_board = generator.fretboard_generator(['e', 'b', 'g', 'd', 'a', 'e'])
+for string in generator.scale_string_translator('e', [0,2,3,5,7,8,10], f_board):
+	print (''.join(string))
+print()
+
+print('arpeggio to practice: c major')
+
+f_board = generator.fretboard_generator(['e', 'b', 'g', 'd', 'a', 'e'])
+for string in generator.scale_string_translator('c', [0, 4, 7], f_board):
+	print (''.join(string))
+print()
+
+print('picking /left hand exercise: ')
+print(generator.permutations_generator())
